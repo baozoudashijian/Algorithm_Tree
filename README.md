e659295732badcd389c43d8aeb14a374131b8788
Roger Zhang <1787009521@qq.com>
2022-08-04 21:34:08 +0800

    feat: 引用地址-浅拷贝-传入参数

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Algorithm_Tree
 树结构相关代码
+
+## 【面试题】扁平结构变成树结构
+flatBecomeTree/1.js
---
+++ flatBecomeTree/1.js
@@ -0,0 +1,23 @@
+const array = [
+    { id: 9, name: "前端基础设施组", parent: 8 },
+    { id: 10, name: "前端业务组", parent: 8 },
+    { id: 7, name: "后端开发部门", parent: 5 },
+    { id: 8, name: "前端开发部门", parent: 5 },
+    { id: 1, name: "CEO", parent: null },
+    { id: 2, name: "运营部", parent: 1 },
+    { id: 3, name: "财务部", parent: 1 },
+    { id: 4, name: "人事部", parent: 1 },
+    { id: 5, name: "技术部", parent: 1 },
+    { id: 6, name: "产品部", parent: 1 }
+];
+
+const array2tree = arr => {
+    const tree = {}
+    const queue = [...arr]
+    queue[0].name = '100'
+    console.log(queue)
+    console.log(arr)
+}
+
+array2tree(array)
+console.log(array)
